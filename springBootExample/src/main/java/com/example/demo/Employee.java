f0383cc57aa36f3b3ad6beb09c070a1ebcf9769a
lakshya03 <42149700+lakshya03@users.noreply.github.com>
2019-08-16 17:31:09 +0530

    Add files via upload

---
+++ springBootExample/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+public class Employee {
+	private long id;
+	private String name;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	
+
+}
